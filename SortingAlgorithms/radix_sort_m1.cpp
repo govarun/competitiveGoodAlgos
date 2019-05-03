798af7e4cc4e73b3953a92342e7f4d381f410a4b
Varun Goyal <varungoyal23@gmail.com>
2019-05-03 17:47:07 +0530

    Optimizations and Naming updates

--- SortingAlgorithms/radix_sort_m1.cpp
+++ SortingAlgorithms/radix_sort_m1.cpp
@@ -11,37 +11,37 @@ int getMaxElement(int arr[], int n){
     return max_element;
 }
 
-
 void countSort(int arr[], int n, int pow10){
-    int freq_end_digit[10]; // To store the count of each end digit
-    memset(freq_end_digit, 0, sizeof(freq_end_digit));
-
+    // End digit refers to the Last digit of a no.
+    // This approach involves storing each element in a vector of vectors according to its last digit
+    // This approach is memory wise inefficient, see the optimized approach.
+    vector <int> end_digit_index[10];
+    
     for(int i = 0; i < n; ++i){
-        freq_end_digit[(arr[i] / pow10) % 10]++;
+        end_digit_index[(arr[i] / pow10) % 10].push_back(i); // Indexes pushed
     }
-    for(int i = 1; i < 10; ++i){
-        freq_end_digit[i] += freq_end_digit[i - 1]; // The prefix sum of the original array
-    }
-
+    
     int arr_sorted[n];
-
-    for(int i = n - 1; i >= 0; --i){
-        arr_sorted[freq_end_digit[(arr[i] / pow10) % 10] - 1] = arr[i]; //  -1 because arrays are 0 indexed.
-        freq_end_digit[(arr[i] / pow10) % 10]--;
-
+    int current_index = 0;  // Current position in the sorted array
+    for(int i = 0; i < 10; ++i){
+        if(!end_digit_index[i].empty()){  // There is at least one element with i as last digit
+            for(int j = 0; j < end_digit_index[i].size(); ++j){
+                arr_sorted[current_index] = arr[end_digit_index[i][j]];
+                ++current_index;
+            }
+        }
     }
 
     for(int i = 0; i < n; ++i){
-        arr[i] = arr_sorted[i]; // Copying the sorted array to the original array
+        arr[i] = arr_sorted[i]; // Copying the sorted array into the original one
     }
 }
 
 
 
 void radixSort(int arr[], int n, int max_element){
-    //Basically we have to sort the array based on the ith digit from the end 
     for(int pow10 = 1; pow10 <= max_element && pow10 <= INT_MAX; pow10 *= 10){
-        countSort(arr, n, pow10);   // Counting sort has to be called for all exponents upto max element
+        countSort(arr, n, pow10);
     }
 
 }
@@ -54,16 +54,15 @@ void printArray(int arr[], int n){
 }
 
 int main(){
-    int n; cin >> n; // n - total no of elements
+    int n; cin >> n; // n -> total no of elements
     int arr[n]; // Assuming the nos to be 32 bit, if they are 64 bit choose long long
 
     for(int i = 0; i < n; ++i){
         cin >> arr[i];
     }
     int max_element = getMaxElement(arr, n);
- 
+
     radixSort(arr, n, max_element);
 
     printArray(arr, n);
-
 }
