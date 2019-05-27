5e4f39e860c8794e79c36a6ee01e2db1d75f9e16
Varun Goyal <42479771+govarun@users.noreply.github.com>
2019-05-27 11:15:47 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,3 +1,4 @@
+//Program.cpp
 //Problem Statement - 
 
 #include <bits/stdc++.h>
@@ -6,11 +7,11 @@
 #include <algorithm>
 using namespace std;
 
-//Author: Varun Goyal: govarun
+//Author: Varun Goyal (@govarun)
 
-#define f(i, n)    for(int i = 0; i < (n); ++i)
-#define fa(i, a, n)  for(int i = (a); i <= (n); ++i)
-#define fd(i, a, n)  for(int i = (a); i >= (n); --i)
+#define f(i, n) for(int i=0;i<(n);++i)
+#define fa(i, a, n) for(int i=(a);i<=(n);++i)
+#define fd(i, a, n) for(int i=(a);i>=(n);--i)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
 #define fill(a)  memset(a, 0, sizeof (a))
@@ -20,31 +21,51 @@ using namespace std;
 #define pb push_back
 #define popb pop_back
 #define ull unsigned long long
-#define DEBUG(x) cout << "> " << #x << ':' << x << endl;
+#define debug if(GOVARUN) cerr
+#define d0(x) debug <<#x<<" = "<<x<<' '
+#define d1(x) debug <<"> "<<#x<<" = "<<x<<'\n'
+#define d2(x, y) debug <<"> "<<#x<<" = "<<x<<' '<<#y<<" = "<<y<<'\n'
+#define d3(x, y, z) debug <<"> "<<#x<<" = "<<x<<' '<<#y<<" = "<<y<<' '<<#z<<" = "<<z<<'\n'
+#define endl '\n'
 typedef long double ld;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
+typedef vector< pair<int, int> > vpii;
 inline bool EQ(double a, double b) { 
 return fabs(a-b) < 1e-9; }
 inline int two(int n) { return 1 << n; }
 
-const ll mod = 1e9+7;
+const ll mod = 1e9 + 7;
+const int maxn = 1e5 + 5;
 
 void pre(){
 
     
 }
 
-void solve(){
 
-
-}
+void solve();
 
 int main(){
-    ios::sync_with_stdio(0); 
+    ios::sync_with_stdio(0);    
     cin.tie(0); cout.tie(0);
-    pre();
-    
+    #ifdef GOVARUN
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #else 
+    #define GOVARUN 0
+    #endif
+    int TC = 1;
+    // cin >> TC;
+    while(TC--){    
+        solve();
+    }
+
     return 0;
+}
+
+void solve(){
+    
+    
 }
