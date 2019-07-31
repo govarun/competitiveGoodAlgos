3f34e76fd27e26252be5925a407253936cbd3794
Udhav Gupta <varungoyal23@gmail.com>
2019-07-31 19:03:53 +0530

    Template added for ease of creating new files

---
+++ SortingAlgorithms/template_varun.cpp
@@ -0,0 +1,75 @@
+//Program.cpp
+//Problem Statement - 
+
+#include <bits/stdc++.h>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+//Author: Varun Goyal (@govarun)
+
+#define f(i, n) for(int i=0;i<(n);++i)
+#define fa(i, a, n) for(int i=(a);i<=(n);++i)
+#define fd(i, a, n) for(int i=(a);i>=(n);--i)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define fill(a)  memset(a, 0, sizeof (a))
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+#define popb pop_back
+#define ull unsigned long long
+#define debug cerr
+#define d0(x) debug <<#x<<" = "<<x<<' '
+#define d1(x) debug <<"> "<<#x<<" = "<<x<<'\n'
+#define d2(x, y) debug <<"> "<<#x<<" = "<<x<<' '<<#y<<" = "<<y<<'\n'
+#define d3(x, y, z) debug <<"> "<<#x<<" = "<<x<<' '<<#y<<" = "<<y<<' '<<#z<<" = "<<z<<'\n'
+#define endl '\n'
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector< pair<int, int> > vpii;
+inline bool EQ(double a, double b) { 
+return fabs(a-b) < 1e-9; }
+inline int two(int n) { return 1 << n; }
+
+const ll mod = 1e9 + 7;
+const int maxn = 1e5 + 5;
+
+// vector <int> adj[maxn];
+// bool vis[maxn];
+// int depth[maxn];
+// int p[maxn];
+// queue <int> q;
+
+void pre(){
+
+    
+}
+
+
+void solve();
+
+int main(){
+    ios::sync_with_stdio(0);    
+    cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int TC = 1;
+    // cin >> TC;
+    while(TC--){    
+        solve();
+    }
+
+    return 0;
+}
+
+void solve(){
+    
+    
+}
