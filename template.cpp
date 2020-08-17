9fb25af9270deec901697d4d2a99975a20b7e794
Varun Goyal <42479771+govarun@users.noreply.github.com>
2020-08-17 15:18:49 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,13 +1,34 @@
-//Program.cpp
-//Problem Statement - 
-
 #include <bits/stdc++.h>
-#include <map>
-#include <unordered_map>
-#include <algorithm>
 using namespace std;
 
 //Author: Varun Goyal (@govarun)
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef GOVARUN
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
 
 #define f(i, n) for(int i=0;i<(n);++i)
 #define fa(i, a, n) for(int i=(a);i<=(n);++i)
@@ -19,33 +40,26 @@ using namespace std;
 #define ss second
 #define mp make_pair
 #define pb push_back
-#define popb pop_back
-#define ull unsigned long long
-#define debug if(GOVARUN) cerr
-#define d0(x) debug <<#x<<" = "<<x<<' '
-#define d1(x) debug <<"> "<<#x<<" = "<<x<<'\n'
-#define d2(x, y) debug <<"> "<<#x<<" = "<<x<<' '<<#y<<" = "<<y<<'\n'
-#define d3(x, y, z) debug <<"> "<<#x<<" = "<<x<<' '<<#y<<" = "<<y<<' '<<#z<<" = "<<z<<'\n'
 #define endl '\n'
-typedef long double ld;
 typedef long long ll;
+typedef long double ld;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
-typedef vector< pair<int, int> > vpii;
-inline bool EQ(double a, double b) { 
-return fabs(a-b) < 1e-9; }
-inline int two(int n) { return 1 << n; }
-
+inline bool EQ(double a, double b) {return fabs(a-b) < 1e-9;}
+ll gcd(ll a, ll b){if(b==0)return a;return gcd(b, a%b);}
+ll lcm(ll a, ll b){return a*(b/gcd(a, b));}
+ll binexp(ll a, ll b, ll mod){ll ans = 1;while(b){if(b&1) ans = ans*a%mod; b/=2;a=a*a%mod;}return ans;}
+ll inverse(ll a, ll p, ll mod){return binexp(a, p-2, mod);}
+// bool sortbysec(const pll &a, const pll &b){return (a.second < b.second);} 
 const ll mod = 1e9 + 7;
 const int maxn = 1e5 + 5;
-
 void pre(){
 
-    
 }
 
-
-void solve();
+void solve() {
+    
+}
 
 int main(){
     ios::sync_with_stdio(0);    
@@ -58,14 +72,11 @@ int main(){
     #endif
     int TC = 1;
     // cin >> TC;
+    pre();
     while(TC--){    
         solve();
+        cout << "\n";
     }
-
     return 0;
 }
 
-void solve(){
-    
-    
-}
